3e8d8677ac5628520ddb3b5e491940c18b4bb380
Simon Balean <simonbalean@gmail.com>
2018-11-14 05:31:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,7 +6,7 @@ def deal_card
   return rand(1..11)
 end
 
-def display_card_total(number)
+def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
 
@@ -28,7 +28,6 @@ def initial_round
   card_total = card1 + card2
   display_card_total
   return display_card_total
-  
 end
 
 def hit?
