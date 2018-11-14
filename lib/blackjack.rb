128bf733504d77c03d12ba470c053ba6d0fb2bf6
Simon Balean <simonbalean@gmail.com>
2018-11-14 05:43:39 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(card_total)
   if input == "h"
     card_total += deal_card
   elsif input == "s"
-  display_card_total
+  display_card_total(card_total)
   else invalid_command
   end
 end
